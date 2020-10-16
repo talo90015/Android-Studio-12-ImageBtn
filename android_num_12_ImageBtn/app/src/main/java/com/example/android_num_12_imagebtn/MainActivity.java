89aaf9c46eab0572b74fa12a0dc04b112e8ca661
PotatoServer <talo90015@gmail.com>
2020-10-16 15:37:47 +0800

    ImageBtn

---
+++ android_num_12_ImageBtn/app/src/main/java/com/example/android_num_12_imagebtn/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android_num_12_imagebtn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //圖片點擊監聽
+        final ImageView imageView = (ImageView)findViewById(R.id.img_btn);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.mipmap.img_b);
+            }
+        });
+    }
+}
